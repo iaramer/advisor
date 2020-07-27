337f4b67acd81c4c8cf677ff8d5fac10ce5ba61e
iaramer <yaramer95@gmail.com>
2020-07-27 22:36:57 +0300

    Added new portfolio balancing module

---
+++ portfolio-balancing-service/src/main/java/ai/adv/portfoliobalancing/PortfolioBalancingServiceApplication.java
@@ -0,0 +1,12 @@
+package ai.adv.portfoliobalancing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBalancingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioBalancingServiceApplication.class, args);
+    }
+}
