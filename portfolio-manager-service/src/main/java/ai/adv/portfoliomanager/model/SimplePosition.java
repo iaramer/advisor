e6efc97cfb3e2deba71856c8f19ccc33c5f8eead
iaramer <yaramer95@gmail.com>
2020-07-31 11:04:46 +0300

    Added or supplemented tables

--- portfolio-manager-service/src/main/java/ai/adv/portfoliomanager/model/SimplePosition.java
+++ portfolio-manager-service/src/main/java/ai/adv/portfoliomanager/model/SimplePosition.java
@@ -10,7 +10,7 @@ import lombok.Setter;
  */
 @Getter
 @Setter
-public class SinglePosition implements Position {
+public class SimplePosition implements Position {
 
   private String ticker;
 
--- portfolio-manager-service/src/main/resources/db/migration/V0__init_schema.sql
+++ portfolio-manager-service/src/main/resources/db/migration/V0__init_schema.sql
@@ -1,5 +1,31 @@
-CREATE TABLE portfolios (
-  id SERIAL UNIQUE,
-  name VARCHAR(64) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE portfolios
+(
+    id   SERIAL UNIQUE,
+    name VARCHAR(64) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE simple_positions
+(
+    id                  SERIAL UNIQUE,
+    portfolio_id        INT        NOT NULL,
+    complex_position_id INT,
+    ticker              VARCHAR(8) NOT NULL,
+    size                INT,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (portfolio_id) REFERENCES portfolios (id),
+    FOREIGN KEY (complex_position_id) REFERENCES complex_positions (id),
+    CHECK (size > 0)
+);
+
+CREATE TABLE complex_positions
+(
+    id           SERIAL UNIQUE,
+    portfolio_id INT NOT NULL,
+    ticker       VARCHAR(8),
+    size         INT,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (portfolio_id) REFERENCES portfolios (id)
 );
