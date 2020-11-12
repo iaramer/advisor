505c4722fe4428b1efd1f6a4c87f8899ce702e19
Iaroslav Amerkhanov <yaramer95@gmail.com>
2020-11-12 18:08:46 +0300

    Added PortfolioController

---
+++ portfolio-manager-service/src/main/java/ai/adv/portfoliomanager/controller/PortfolioController.java
@@ -0,0 +1,21 @@
+package ai.adv.portfoliomanager.controller;
+
+import ai.adv.portfoliomanager.dto.PortfolioDto;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/portfolios")
+public class PortfolioController {
+
+  private final PortfolioService portfolioService;
+
+  @GetMapping("/all")
+  public List<PortfolioDto> getAllPortfolios() {
+    return portfolioService.getAllPortfolios();
+  }
+}
