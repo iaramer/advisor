d9eb329b5a557f072b4faa0aa28143b552388493
iaramer <yaramer95@gmail.com>
2020-12-08 23:47:33 +0300

    Finished Balancer. Added unit-tests

--- portfolio-management-lib/src/main/java/balancing/Balancer.java
+++ portfolio-management-lib/src/main/java/balancing/Balancer.java
@@ -2,6 +2,8 @@ package balancing;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -9,6 +11,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 
 @RequiredArgsConstructor
 public class Balancer {
@@ -18,7 +21,7 @@ public class Balancer {
   /**
    * Формирует портфель. Возвращает мапу "тикер"-"колличество акций". Не зависит от валюты актива.
    */
-  public Map<String, Integer> formPortfolio(Map<String, BigDecimal> modelPortfolio,
+  public Map<String, BigDecimal> formPortfolio(Map<String, BigDecimal> modelPortfolio,
       Map<String, BigDecimal> prices, BigDecimal cashValue) {
 
     if (modelPortfolio.isEmpty() || cashValue.equals(BigDecimal.ZERO)) {
@@ -30,26 +33,79 @@ public class Balancer {
 
     //todo: check all prices exist
 
-    Map<String, Integer> portfolio = new HashMap<>();
+    Map<String, BigDecimal> portfolio = new HashMap<>();
 
     BigDecimal valueRemainder = cashValue;
-    List<Entry<String, BigDecimal>> modelPortfolioEntries = modelPortfolio.entrySet().stream()
-        .sorted(Entry.comparingByValue()) //todo: check the order is really DESC. We need the highest proportions to go first
-        .collect(Collectors.toList());
+    List<Entry<String, BigDecimal>> modelPortfolioEntries = sort(modelPortfolio);
+    List<Entry<String, BigDecimal>> normalizedModelPortfolioEntries = normalize(
+        modelPortfolioEntries);
 
-    for (Entry<String, BigDecimal> entry : modelPortfolioEntries) {
+    for (Entry<String, BigDecimal> entry : normalizedModelPortfolioEntries) {
       if (entry.getValue().compareTo(BigDecimal.ZERO) <= 0) {
         continue;
       }
-      BigDecimal exactPositionValue = entry.getValue().multiply(cashValue);
-      BigDecimal stockPrice = prices.get(entry.getKey());
-      BigDecimal numberOfSecurities = exactPositionValue.divide(stockPrice, RoundingMode.FLOOR);
+      BigDecimal exactPositionValue = cashValue.multiply(entry.getValue());
+      BigDecimal securityPrice = prices.get(entry.getKey());
+      BigDecimal initialSecuritiesNumber = exactPositionValue
+          .divide(securityPrice, 0, RoundingMode.FLOOR);
 
-      BigDecimal newPositionValue = stockPrice.multiply(numberOfSecurities);
+      BigDecimal newPositionValue = securityPrice.multiply(initialSecuritiesNumber);
       valueRemainder = valueRemainder.subtract(newPositionValue);
-      portfolio.put(entry.getKey(), numberOfSecurities.intValueExact());
+      portfolio.put(entry.getKey(), initialSecuritiesNumber.setScale(0, RoundingMode.DOWN));
+    }
+
+    for (Entry<String, BigDecimal> entry : normalizedModelPortfolioEntries) {
+      BigDecimal securityPrice = prices.get(entry.getKey());
+      BigDecimal additionalSecuritiesNumber = valueRemainder
+          .divide(securityPrice, 0, RoundingMode.FLOOR);
+      if (additionalSecuritiesNumber.compareTo(BigDecimal.ZERO) <= 0) {
+        continue;
+      }
+
+      BigDecimal initialSecuritiesNumber = portfolio.get(entry.getKey()); //todo: what if there's no such key in portfolio from the first iteration
+      BigDecimal finalSecuritiesNumber = additionalSecuritiesNumber.add(initialSecuritiesNumber);
+      valueRemainder = valueRemainder.subtract(finalSecuritiesNumber.multiply(securityPrice));
+      portfolio.put(entry.getKey(), finalSecuritiesNumber.setScale(0, RoundingMode.DOWN));
+    }
+
+    if (valueRemainder.compareTo(BigDecimal.ZERO) > 0) {
+      portfolio.put(currencyTicker, valueRemainder);
     }
 
     return portfolio;
+  }
+
+  private List<Entry<String, BigDecimal>> sort(Map<String, BigDecimal> modelPortfolio) {
+    return modelPortfolio.entrySet().stream()
+        .filter(this::isNotEmptyOrZero)
+        .sorted(Entry
+            .comparingByValue()) //todo: check the order is really DESC. We need the highest proportions to go first
+        .collect(Collectors.toList());
+  }
+
+  private List<Entry<String, BigDecimal>> normalize(
+      List<Entry<String, BigDecimal>> modelPortfolioEntries) {
+    if (modelPortfolioEntries.isEmpty()) {
+      return Collections.emptyList();
+    }
+
+    BigDecimal summaryShares = BigDecimal.ZERO;
+    List<Entry<String, BigDecimal>> normalizedEntries = new ArrayList<>();
+    for (Entry<String, BigDecimal> entry : modelPortfolioEntries) {
+      summaryShares = summaryShares.add(entry.getValue());
+    }
+    for (Entry<String, BigDecimal> entry : modelPortfolioEntries) {
+      BigDecimal percentage = entry.getValue().divide(summaryShares, 3, RoundingMode.FLOOR);
+      normalizedEntries.add(new AbstractMap.SimpleEntry<>(entry.getKey(), percentage));
+    }
+    return normalizedEntries;
+  }
+
+  private boolean isNotEmptyOrZero(Entry<String, BigDecimal> entry) {
+    String ticker = entry.getKey();
+    BigDecimal number = entry.getValue();
+    return StringUtils.isNoneBlank(ticker)
+        && number != null
+        && number.compareTo(BigDecimal.ZERO) > 0;
   }
 }
--- portfolio-management-lib/src/test/java/balancing/BalancerTest.java
+++ portfolio-management-lib/src/test/java/balancing/BalancerTest.java
@@ -2,6 +2,7 @@ package balancing;
 
 import java.math.BigDecimal;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -25,7 +26,7 @@ class BalancerTest {
     Map<String, BigDecimal> prices = Collections.emptyMap();
     BigDecimal cashValue = BigDecimal.ZERO;
 
-    Map<String, Integer> portfolio = balancer.formPortfolio(modelPortfolio, prices, cashValue);
+    Map<String, BigDecimal> portfolio = balancer.formPortfolio(modelPortfolio, prices, cashValue);
 
     Assertions.assertEquals(Collections.emptyMap(), portfolio);
   }
@@ -33,17 +34,17 @@ class BalancerTest {
   @Test
   void formPortfolio_EmptyModelPortfolioNonZeroPricesAndCash_EmptyMap() {
     Map<String, BigDecimal> modelPortfolio = Collections.emptyMap();
-    Map<String, BigDecimal> prices = Collections.singletonMap("USD", BigDecimal.ONE);
+    Map<String, BigDecimal> prices = Collections.singletonMap("MSFT", BigDecimal.ONE);
     BigDecimal cashValue = BigDecimal.ONE;
 
-    Map<String, Integer> portfolio = balancer.formPortfolio(modelPortfolio, prices, cashValue);
+    Map<String, BigDecimal> portfolio = balancer.formPortfolio(modelPortfolio, prices, cashValue);
 
     Assertions.assertEquals(Collections.emptyMap(), portfolio);
   }
 
   @Test
   void formPortfolio_EmptyPricesNonEmptyModelPortfolioAndCash_EmptyMap() {
-    Map<String, BigDecimal> modelPortfolio = Collections.singletonMap("USD", BigDecimal.ONE);
+    Map<String, BigDecimal> modelPortfolio = Collections.singletonMap("MSFT", BigDecimal.ONE);
     Map<String, BigDecimal> prices = Collections.emptyMap();
     BigDecimal cashValue = BigDecimal.ONE;
 
@@ -52,14 +53,14 @@ class BalancerTest {
   }
 
   @Test
-  void formPortfolio_ModelPortfolioWithZeroProportion_EmptyMap() {
-    Map<String, BigDecimal> modelPortfolio = Collections.singletonMap("USD", BigDecimal.ZERO);
-    Map<String, BigDecimal> prices = Collections.singletonMap("USD", BigDecimal.ONE);
+  void formPortfolio_ModelPortfolioWithZeroProportion_MapWithCash() {
+    Map<String, BigDecimal> modelPortfolio = Collections.singletonMap("MSFT", BigDecimal.ZERO);
+    Map<String, BigDecimal> prices = Collections.singletonMap("MSFT", BigDecimal.ONE);
     BigDecimal cashValue = BigDecimal.ONE;
 
-    Map<String, Integer> portfolio = balancer.formPortfolio(modelPortfolio, prices, cashValue);
+    Map<String, BigDecimal> portfolio = balancer.formPortfolio(modelPortfolio, prices, cashValue);
 
-    Assertions.assertEquals(Collections.emptyMap(), portfolio);
+    Assertions.assertEquals(Collections.singletonMap("USD", BigDecimal.ONE), portfolio);
   }
 
   @Test
@@ -68,9 +69,67 @@ class BalancerTest {
     Map<String, BigDecimal> prices = Collections.singletonMap("MSFT", BigDecimal.ONE);
     BigDecimal cashValue = BigDecimal.ONE;
 
-    Map<String, Integer> portfolio = balancer.formPortfolio(modelPortfolio, prices, cashValue);
+    Map<String, BigDecimal> portfolio = balancer.formPortfolio(modelPortfolio, prices, cashValue);
 
-    Assertions.assertEquals(Collections.singletonMap("MSFT", 1), portfolio);
+    Assertions.assertEquals(Collections.singletonMap("MSFT", BigDecimal.ONE), portfolio);
+  }
+
+  @Test
+  void formPortfolio_ModelPortfolioWithTwoEqualProportions_MapWithTwoPositions() {
+    Map<String, BigDecimal> modelPortfolio = new HashMap<>();
+    modelPortfolio.put("AAPL", new BigDecimal("0.5"));
+    modelPortfolio.put("MSFT", new BigDecimal("0.5"));
+    Map<String, BigDecimal> prices = new HashMap<>();
+    prices.put("AAPL", BigDecimal.TEN);
+    prices.put("MSFT", BigDecimal.TEN);
+    BigDecimal cashValue = new BigDecimal("20");
+
+    Map<String, BigDecimal> portfolio = balancer.formPortfolio(modelPortfolio, prices, cashValue);
+
+    Map<String, BigDecimal> expectedPortfolio = new HashMap<>();
+    expectedPortfolio.put("AAPL", BigDecimal.ONE);
+    expectedPortfolio.put("MSFT", BigDecimal.ONE);
+    Assertions.assertEquals(expectedPortfolio, portfolio);
+  }
+
+  @Test
+  void formPortfolio_ModelPortfolioWithTwoEqualShares_MapWithTwoPositions() {
+    Map<String, BigDecimal> modelPortfolio = new HashMap<>();
+    modelPortfolio.put("AAPL", BigDecimal.ONE);
+    modelPortfolio.put("MSFT", BigDecimal.ONE);
+    Map<String, BigDecimal> prices = new HashMap<>();
+    prices.put("AAPL", BigDecimal.TEN);
+    prices.put("MSFT", BigDecimal.TEN);
+    BigDecimal cashValue = new BigDecimal("20");
+
+    Map<String, BigDecimal> portfolio = balancer.formPortfolio(modelPortfolio, prices, cashValue);
+
+    Map<String, BigDecimal> expectedPortfolio = new HashMap<>();
+    expectedPortfolio.put("AAPL", BigDecimal.ONE);
+    expectedPortfolio.put("MSFT", BigDecimal.ONE);
+    Assertions.assertEquals(expectedPortfolio, portfolio);
+  }
+
+  @Test
+  void formPortfolio_ModelPortfolioWithThreeEqualShares_MapWithThreePositions() {
+    Map<String, BigDecimal> modelPortfolio = new HashMap<>();
+    modelPortfolio.put("AAPL", BigDecimal.ONE);
+    modelPortfolio.put("MSFT", BigDecimal.ONE);
+    modelPortfolio.put("GOOGL", BigDecimal.ONE);
+    Map<String, BigDecimal> prices = new HashMap<>();
+    prices.put("AAPL", BigDecimal.TEN);
+    prices.put("MSFT", BigDecimal.TEN);
+    prices.put("GOOGL", BigDecimal.TEN);
+    BigDecimal cashValue = new BigDecimal("31");
+
+    Map<String, BigDecimal> portfolio = balancer.formPortfolio(modelPortfolio, prices, cashValue);
+
+    Map<String, BigDecimal> expectedPortfolio = new HashMap<>();
+    expectedPortfolio.put("AAPL", BigDecimal.ONE);
+    expectedPortfolio.put("MSFT", BigDecimal.ONE);
+    expectedPortfolio.put("GOOGL", BigDecimal.ONE);
+    expectedPortfolio.put("USD", BigDecimal.ONE);
+    Assertions.assertEquals(expectedPortfolio, portfolio);
   }
 
 }
