aabde3d2da19500962df7e7405de72a87b226388
iaramer <yaramer95@gmail.com>
2020-12-29 01:59:42 +0300

    Added cron messaging to kafka

--- data-service/src/main/java/ai/adv/data/DataServiceApplication.java
+++ data-service/src/main/java/ai/adv/data/DataServiceApplication.java
@@ -2,8 +2,9 @@ package ai.adv.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class DataServiceApplication {
 
   public static void main(String[] args) {
--- data-service/src/main/java/ai/adv/data/kafka/StockPriceProducerService.java
+++ data-service/src/main/java/ai/adv/data/kafka/StockPriceProducerService.java
@@ -1,14 +1,18 @@
 package ai.adv.data.kafka;
 
 import ai.adv.data.dto.StockPriceDto;
+import java.math.BigDecimal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-@Service
 @Slf4j
+@Service
+@EnableScheduling
 @RequiredArgsConstructor
 public class StockPriceProducerService {
 
@@ -16,6 +20,13 @@ public class StockPriceProducerService {
   private String topic;
 
   private final KafkaTemplate<Object, StockPriceDto> kafkaTemplate;
+
+  @Scheduled(cron = "${cron.refresh.stock-prices}")
+  public void testPublish() {
+    log.info("Test publishing");
+    StockPriceDto stockPriceDto = new StockPriceDto("MAMO", BigDecimal.TEN);
+    publish(stockPriceDto);
+  }
 
   public void publish(StockPriceDto stockPriceDto) {
     kafkaTemplate.send(topic, stockPriceDto);
