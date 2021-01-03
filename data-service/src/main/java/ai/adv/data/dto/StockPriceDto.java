c416dff6576715e0636de993182ffe4a389ffee8
iaramer <yaramer95@gmail.com>
2021-01-03 19:59:23 +0300

    Added kafka consumer in portfolio-manager-service

--- data-service/src/main/java/ai/adv/data/dto/StockPriceDto.java
+++ data-service/src/main/java/ai/adv/data/dto/StockPriceDto.java
@@ -3,8 +3,10 @@ package ai.adv.data.dto;
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class StockPriceDto {
 
